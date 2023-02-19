a1b9d9cca16f005086803d6c3f25305798daefdf
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-02-19 20:28:23 +0100

    3

--- server/index.js
+++ server/index.js
@@ -18,6 +18,6 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => res.send('INDEX'))
-
+//testar
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
